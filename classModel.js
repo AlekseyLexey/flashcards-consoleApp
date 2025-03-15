b5b2c662d9d4cb0aa609c30cd713805919f1a71a
Faceless Friend <pelk@ya.ru>
2025-03-15 14:47:00 +0300

    fixed

--- classModel.js
+++ classModel.js
@@ -2,33 +2,30 @@ const fs = require('fs').promises;
 const { EOL } = require('os');
 
 export class Model {
+  constructor() {
+    this.questions = [];
+  }
   getThemes() {
     return fs.readdir('./topics')
-      .then((dirs) => dirs)
-      .catch((err) => {
-        console.error('Ошибка:', err);
-      });
   }
-  setQuestionsData(number) {
-    return this.getThemes()
-      .then((dirs) => {
-        if (number < 1 || number > dirs.length) {
-          throw new Error('Неверный номер темы');
-        }
 
-        const dir = dirs[number - 1];
+  async getQuestions() {
+     return this.questions;
+  }
 
-        return fs.readFile(`./topics/${dir}`, 'utf-8');
-      })
-      .then((data) => {
-        return data.split(`${EOL}${EOL}`).map((el) => {
-          const [question, answer] = el.split(EOL);
-          return { question: question.trim(), answer: answer.trim() };
-        });
-      })
-      .catch((err) => {
-        console.error('Ошибка:', err);
-        throw err;
-      });
+  async setQuestionsData(number) {
+    const dirs = await this.getThemes();
+
+    if (number < 1 || number > dirs.length) {
+      throw new Error('Неверный номер темы');
+    }
+
+    const dir = dirs[number - 1];
+    const data = await fs.readFile(`./topics/${dir}`, 'utf-8');
+
+    return data.split(`${EOL}${EOL}`).map((el) => {
+      const [question, answer] = el.split(EOL);
+      return { question: question.trim(), answer: answer.trim() };
+    });
   }
 }
